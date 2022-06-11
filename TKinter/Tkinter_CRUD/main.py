0806739aa2586e36f0bb40a1ca457a5f330eac2c
Luis Felipe Caretta <lcaretta@yahoo.com>
2022-06-11 10:34:58 -0300

    Tema "Yaru" adicionado.

--- TKinter/Tkinter_CRUD/main.py
+++ TKinter/Tkinter_CRUD/main.py
@@ -3,6 +3,7 @@ from tkcalendar import Calendar, DateEntry
 from tkinter import *
 from arquivos.view import *
 from tkinter import messagebox
+from ttkthemes import ThemedTk
 
 #Tabela de cores
 co0 = "#f0f3f5"  # Preta
@@ -16,24 +17,27 @@ co7 = "#ef5350"   # vermelha
 co8 = "#263238"   # + verde
 co9 = "#e9edf5"   # sky blue
 
-janela = Tk()
+
+janela = ThemedTk(theme="yaru")
 janela.title("Minha Biblioteca")
-janela.geometry("1000x450")
+janela.geometry("1005x450")
 janela.configure(background=co1)
 janela.resizable(width=False, height=False)
 
+
+
 #Divisão da tela
 #Parte de cima label_cima
-frame_cima = Frame(janela, width=300, height=50, bg=co9, relief="sunken")
+frame_cima = Frame(janela, width=300, height=50, relief="flat")
 frame_cima.grid(row=0, column=0)
 
-frame_baixo = Frame(janela, width=300, height=400, bg=co9, relief="flat")
+frame_baixo = Frame(janela, width=300, height=400, relief="flat")
 frame_baixo.grid(row=1, column=0, sticky=NSEW, padx=0, pady=1)
 
-frame_direita = Frame(janela, width=1000, height=400, bg=co1, relief="flat")
+frame_direita = Frame(janela, width=1000, height=400, relief="flat")
 frame_direita.grid(row=0, column=1, rowspan=2, padx=1, pady=0,sticky=NSEW)
 
-app_nome = Label(frame_cima, text="Minha Biblioteca",   anchor=NW, font=("Ivy 20 bold"), bg=co9, fg=co3, relief="flat")
+app_nome = Label(frame_cima, text="Minha Biblioteca", anchor=NW, font=("Ivy 20 bold"), fg=co3, relief="flat")
 app_nome.place(x=30, y=10)
 
 global tree
@@ -137,38 +141,38 @@ def deletar():
 
 #Parte de baixo label_baixo
 #Nome do livro
-l_nome = Label(frame_baixo, text="Nome do livro",   anchor=NW, font=("Times 12 bold"), bg=co9, fg=co4, relief="flat")
+l_nome = Label(frame_baixo, text="Nome do livro", anchor=NW, font=("Times 12 bold"), fg=co4, relief="flat")
 l_nome.place(x=30, y=0)
 
 l_entry = Entry(frame_baixo, width=25, font=("Times 16 bold"), justify="left", relief="solid")
 l_entry.place(x=13, y=30)
 
 #Descrição do livro
-descricao = Label(frame_baixo, text="Descrição",   anchor=NW, font=("Times 12 bold"), bg=co9, fg=co4, relief="flat")
+descricao = Label(frame_baixo, text="Descrição", anchor=NW, font=("Times 12 bold"), fg=co4, relief="flat")
 descricao.place(x=30, y=70)
 
 descricao_entry = Entry(frame_baixo, width=25, font=("Times 16 bold"), justify="left", relief="solid")
 descricao_entry.place(x=13, y=100)
 
 #Data de início e final
-data_i = Label(frame_baixo, text="Início", anchor=NW, font=("Times 12 bold"), bg=co9, fg=co4, relief="flat")
+data_i = Label(frame_baixo, text="Início", anchor=NW, font=("Times 12 bold"), fg=co4, relief="flat")
 data_i.place(x=30, y=140)
 data_entry_i = DateEntry(frame_baixo, width=25,background="gray", foreground="white", borderwidth=2)
 data_entry_i.place(x=13, y=170)
 
-data_f = Label(frame_baixo, text="Final", anchor=NW, font=("Times 12 bold"), bg=co9, fg=co4, relief="flat")
-data_f.place(x=30, y=200)
+data_f = Label(frame_baixo, text="Final", anchor=NW, font=("Times 12 bold"), fg=co4, relief="flat")
+data_f.place(x=30, y=220)
 data_entry_f = DateEntry(frame_baixo, width=25, background="gray", foreground="white", borderwidth=2)
-data_entry_f.place(x=13, y=230)
+data_entry_f.place(x=13, y=250)
 
 #Botões
-btn_inserir = Button(frame_baixo, command=inserir, text="Inserir", width=10, font=("Times 10 bold"), bg=co2, fg=co1, relief="solid", overrelief="ridge")
+btn_inserir = Button(frame_baixo, command=inserir, text="Inserir", width=10, font=("Times 10 bold"), bg=co2, fg=co1, relief="raised", overrelief="groove")
 btn_inserir.place(x=13, y=340)
 
-btn_atualizar = Button(frame_baixo, command=atualizar, text="Atualizar", width=10, font=("Times 10 bold"), bg=co6, fg=co1, relief="solid", overrelief="ridge")
+btn_atualizar = Button(frame_baixo, command=atualizar, text="Atualizar", width=10, font=("Times 10 bold"), bg=co6, fg=co1, relief="raised", overrelief="groove")
 btn_atualizar.place(x=103, y=340)
 
-btn_apagar = Button(frame_baixo, command=deletar, text="Apagar", width=10, font=("Times 10 bold"), bg=co7, fg=co1, relief="solid", overrelief="ridge")
+btn_apagar = Button(frame_baixo, command=deletar, text="Apagar", width=10, font=("Times 10 bold"), bg=co7, fg=co1, relief="raised", overrelief="groove")
 btn_apagar.place(x=193, y=340)
 
 #Frame direita
