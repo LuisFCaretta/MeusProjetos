e1aef173c37347f2bf14bf68f7270bf893927200
Luis Felipe Caretta <lcaretta@yahoo.com>
2022-06-14 09:18:57 -0300

    Backend e Frontend de login

--- TKinter/Tkinter_CRUD/arquivos/login.py
+++ TKinter/Tkinter_CRUD/arquivos/login.py
@@ -1,10 +1,10 @@
 from tkinter import messagebox
 import mysql.connector
 import customtkinter
+from hashlib import sha256
 
 customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("blue")
-
 janela = customtkinter.CTk()
 janela.title("Minha biblioteca login")
 label = customtkinter.CTkLabel(janela)
@@ -17,54 +17,71 @@ frame_cima.grid(row=0, column=0)
 frame_baixo = customtkinter.CTkFrame(janela, width=300, height=350, relief="flat", border=1)
 frame_baixo.grid(row=1, column=0, padx=0, pady=1)
 
-frame_nome = customtkinter.CTkLabel(frame_cima, text="Login", bg_color="#348feb", anchor="center", text_font=("Arial 24 bold"))
-frame_nome.place(x=75, y=5)
+def telaLogin():
+    global janela
+    janela.destroy()
+    janela = customtkinter.CTkToplevel()
+    janela.title("Minha biblioteca login")
+    label = customtkinter.CTkLabel(janela)
+    label.place()
+    janela.geometry("300x400")
+    janela.resizable(width=False, height=False)
 
-l_email = customtkinter.CTkLabel(frame_baixo, text="Email", width=80, anchor="nw", text_font=("Arial 18 bold"))
-l_email.place(x=10, y=20)
+    frame_cima = customtkinter.CTkFrame(janela, width=300, height=50, fg_color="#348feb", border=1)
+    frame_cima.grid(row=0, column=0)
+    frame_baixo = customtkinter.CTkFrame(janela, width=300, height=350, relief="flat", border=1)
+    frame_baixo.grid(row=1, column=0, padx=0, pady=1)
 
-email_entry = customtkinter.CTkEntry(frame_baixo, width=260, justify="left", relief="raised")
-email_entry.place(x=20, y=60)
+    frame_nome = customtkinter.CTkLabel(frame_cima, text="Login", bg_color="#348feb", anchor="center", text_font=("Arial 24 bold"))
+    frame_nome.place(x=75, y=5)
 
-l_senha = customtkinter.CTkLabel(frame_baixo, text="Senha", width=80, anchor="nw", text_font=("Arial 18 bold"))
-l_senha.place(x=10, y=100)
+    l_email = customtkinter.CTkLabel(frame_baixo, text="Email", width=80, anchor="nw", text_font=("Arial 18 bold"))
+    l_email.place(x=10, y=20)
 
-senha_entry = customtkinter.CTkEntry(frame_baixo, width=260, justify="left", relief="raised", show="*")
-senha_entry.place(x=20, y=140)
+    email_entry = customtkinter.CTkEntry(frame_baixo, width=260, justify="left", relief="raised")
+    email_entry.place(x=20, y=60)
 
-def verifica_login():
-    email = email_entry.get()
-    senha = senha_entry.get()
-    if email == "" or senha == "":
-        messagebox.showerror('Erro!', 'Os campos não podem ser vazios!')
-        return
-    ver = [email, senha]
-    con = mysql.connector.connect(host='localhost', database='db_user_admin', user='root', password='', autocommit=True, use_unicode=True)
-    cursor = con.cursor(buffered=True)
-    cursor.execute("select email_admin, senha_admin, nome_admin from admin")
-    verifica = cursor.fetchall()
-    for admin in verifica:
-        try:
-            if admin[0] == ver[0] and admin[1] == ver[1]:
-                messagebox.showinfo('Sucesso!', f'Seja bem vindo(a), {admin[2]}!')
-                ...
-                
-            if admin[0] == ver[0] and admin[1] != ver[1]:
-                messagebox.showerror("Erro", "Senha incorreta, verifique e tente novamente!")
-                return
-            if admin[0] != ver[0] and admin[1] == ver[1]:
-                messagebox.showerror("Erro", "Email incorreto, verifique e tente novamente!")
-                return
-        except:
+    l_senha = customtkinter.CTkLabel(frame_baixo, text="Senha", width=80, anchor="nw", text_font=("Arial 18 bold"))
+    l_senha.place(x=10, y=100)
+
+    senha_entry = customtkinter.CTkEntry(frame_baixo, width=260, justify="left", relief="raised", show="*")
+    senha_entry.place(x=20, y=140)
+
+    def verifica_login():
+        email = email_entry.get()
+        senha = senha_entry.get()
+        senha = sha256(senha.encode()).hexdigest()
+        if email == "" or senha == "":
+            messagebox.showerror('Erro!', 'Os campos não podem ser vazios!')
             return
-    if ver[0] not in admin[0]:
-        messagebox.showerror("Erro", "Você ainda não possui cadastro, verifique e tente novamente!")
-    return
+        ver = [email, senha]
+        con = mysql.connector.connect(host='localhost', database='db_user_admin', user='root', password='', autocommit=True, use_unicode=True)
+        cursor = con.cursor(buffered=True)
+        cursor.execute("select email_admin, senha_admin, nome_admin from admin")
+        verifica = cursor.fetchall()
+        for admin in verifica:
+            try:
+                if admin[0] == ver[0] and admin[1] == ver[1]:
+                    messagebox.showinfo('Sucesso!', f'Seja bem vindo(a), {admin[2]}!')
+                    ...
+                    
+                if admin[0] == ver[0] and admin[1] != ver[1]:
+                    messagebox.showerror("Erro", "Senha incorreta, verifique e tente novamente!")
+                    return
+                if admin[0] != ver[0] and admin[1] == ver[1]:
+                    messagebox.showerror("Erro", "Email incorreto, verifique e tente novamente!")
+                    return
+            except:
+                return
+        if ver[0] not in admin[0]:
+            messagebox.showerror("Erro", "Você ainda não possui cadastro, verifique e tente novamente!")
+        return
 
-btn_login = customtkinter.CTkButton(frame_baixo, text="Login", text_font=("Arial 24 bold"), width=200, fg_color="#348feb", height=10, command=verifica_login)
-btn_login.place(x=50, y=200)
+    btn_login = customtkinter.CTkButton(frame_baixo, text="Login", text_font=("Arial 24 bold"), width=200, fg_color="#348feb", height=10, command=verifica_login)
+    btn_login.place(x=50, y=200)
 
 def telaCadastro():
+    global frame_baixo
     global janela
     janela.destroy()
     janela = customtkinter.CTkToplevel()
@@ -104,10 +121,46 @@ def telaCadastro():
     senha2_entry = customtkinter.CTkEntry(frame_baixo, width=260, justify="left", relief="raised", show="*")
     senha2_entry.place(x=20, y=280)
 
-    btn_cadastrar = customtkinter.CTkButton(frame_baixo, text="Cadastrar", fg_color="#348feb", text_font=("Arial 24 bold"), width=200, height=10)
+    def executar_cadastro():
+        nome = l_nome_entry.get()
+        email = email_entry.get()
+        senha = senha_entry.get()
+        senha2 = senha2_entry.get()
+
+        if nome == "" or email == "" or senha == "" or senha2 == "":
+            messagebox.showerror('Erro!', 'Os campos não podem ser vazios!')
+            return
+        if senha != senha2:
+            messagebox.showerror('Erro!', 'As senhas devem ser iguais, tente novamente.')
+            return
+        
+        senha = sha256(senha.encode()).hexdigest()
+        novo = [nome, email, senha]
+        
+        con = mysql.connector.connect(host='localhost', database='db_user_admin', user='root', password='', autocommit=True, use_unicode=True)
+        cursor = con.cursor(buffered=True)
+        cursor.execute("use db_user_admin;")
+        cursor.execute("select * from admin;")
+        verifica = cursor.fetchall()
+        for admin in verifica:
+            if novo[1] == admin[2]:
+                messagebox.showwarning('Erro!', f'Já existe o email {admin[2]} no nosso sistema!')
+                return
+            if novo[1] != admin[2]:
+                pass
+        cursor.execute(f"INSERT IGNORE INTO admin VALUES(default, '{novo[0]}', '{novo[1]}', '{novo[2]}');")
+        messagebox.showinfo('Parabéns!', f'Cadastro realizado com sucesso, {novo[0]}!')
+
+    btn_cadastrar = customtkinter.CTkButton(frame_baixo, text="Cadastrar", fg_color="#348feb", text_font=("Arial 24 bold"), width=200, height=10, command=executar_cadastro)
     btn_cadastrar.place(x=50, y=340)
 
+    btn_voltar = customtkinter.CTkButton(frame_baixo, text="Ir para login", text_font=("Arial 18 bold"), width=150, fg_color="#348feb", height=10, command=telaLogin)
+    btn_voltar.place(x=75, y=400)
+
+btn_login = customtkinter.CTkButton(frame_baixo, text="Login", text_font=("Arial 24 bold"), width=200, fg_color="#348feb", height=10, command=telaLogin)
+btn_login.place(x=50, y=100)
+
 btn_cadastrar = customtkinter.CTkButton(frame_baixo, text="Cadastrar", width=200, fg_color="#348feb", text_font=("Arial 24 bold"), height=10, command=telaCadastro)
-btn_cadastrar.place(x=50, y=280)
+btn_cadastrar.place(x=50, y=200)
 
 janela.mainloop()
